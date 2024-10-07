e6a745007dd559874e43597542b4f877c1b30503
m5s3 <devmyoungsoo2@gmail.com>
2024-10-07 14:55:46 +0900

    init

---
+++ src/main/java/com/consertreservation/ConsertReservationApplication.java
@@ -0,0 +1,13 @@
+package com.consertreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsertReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsertReservationApplication.class, args);
+    }
+
+}

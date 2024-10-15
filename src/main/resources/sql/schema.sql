8b3bac1ad1231da78f556619346f1eef88eb5e51
m5s3 <devmyoungsoo2@gmail.com>
2024-10-15 10:35:50 +0900

    fix: schema 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -13,6 +13,7 @@ COMMENT '유저';
 CREATE TABLE `user_token`
 (
     `user_token_id`     BINARY(16) NOT NULL,
+    `user_id`           BIGINT(20) NOT NULL,
     `waiting_order`     INT(20) NOT NULL COMMENT '대기열 순서',
     `status`            VARCHAR(20) NOT NULL COMMENT '대기열 상태',
     `created_at`        datetime(6) NOT NULL COMMENT '생성 일시',

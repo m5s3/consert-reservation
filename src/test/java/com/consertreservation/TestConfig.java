bb1a2b155c24ea4d0b44c63edb31a69184a0b056
m5s3 <devmyoungsoo2@gmail.com>
2024-10-15 16:57:08 +0900

    feat: test 설정 적용

--- src/test/java/com/consertreservation/TestConfig.java
+++ src/test/java/com/consertreservation/TestConfig.java
@@ -1,4 +1,13 @@
 package com.consertreservation;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ActiveProfiles("test")
+@SpringBootTest(classes = ConcertReservationApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql({"/sql/schema.sql"})
 public class TestConfig {
 }

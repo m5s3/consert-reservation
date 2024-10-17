d1d453deaaa7c6d343f025554477b53976899398
m5s3 <devmyoungsoo2@gmail.com>
2024-10-17 22:20:04 +0900

    fix(ReserveSeatUseCase): @Transactional 추가

--- src/main/java/com/consertreservation/api/usecase/ReserveSeatUseCase.java
+++ src/main/java/com/consertreservation/api/usecase/ReserveSeatUseCase.java
@@ -7,9 +7,11 @@ import com.consertreservation.domain.usertoken.components.UserTokenComponent;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReserveSeatUseCase {
 
     private final ConcertScheduleComponent concertScheduleComponent;

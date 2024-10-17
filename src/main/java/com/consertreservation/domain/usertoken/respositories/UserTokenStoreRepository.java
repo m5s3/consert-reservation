19169850d233b6642f7180e4968f1d864d3e3b9d
m5s3 <devmyoungsoo2@gmail.com>
2024-10-18 01:19:52 +0900

    refactor: 미사용 임포트 제거

--- src/main/java/com/consertreservation/domain/usertoken/respositories/UserTokenStoreRepository.java
+++ src/main/java/com/consertreservation/domain/usertoken/respositories/UserTokenStoreRepository.java
@@ -1,6 +1,5 @@
 package com.consertreservation.domain.usertoken.respositories;
 
-import com.consertreservation.domain.user.User;
 import com.consertreservation.domain.usertoken.model.UserToken;
 
 public interface UserTokenStoreRepository {

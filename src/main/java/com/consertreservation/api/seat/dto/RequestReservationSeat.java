6bc76d1a6b25234d63cb7d33dff95d0569ee2145
m5s3 <devmyoungsoo2@gmail.com>
2024-10-17 20:44:08 +0900

    feat(RequestReservationSeat): 콘서트 ID 추가

--- src/main/java/com/consertreservation/api/seat/dto/RequestReservationSeat.java
+++ src/main/java/com/consertreservation/api/seat/dto/RequestReservationSeat.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public record RequestReservationSeat(
         Long seatId,
         Long userId,
+        Long concertId,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime reservationDate
 ) {

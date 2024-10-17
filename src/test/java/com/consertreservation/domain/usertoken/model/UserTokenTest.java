6d0ea222048b24414a0ce63da88e418c5745776c
m5s3 <devmyoungsoo2@gmail.com>
2024-10-18 03:17:33 +0900

    test(UserToken): 권한 테스트 케이스 추가

--- src/test/java/com/consertreservation/domain/usertoken/model/UserTokenTest.java
+++ src/test/java/com/consertreservation/domain/usertoken/model/UserTokenTest.java
@@ -1,6 +1,7 @@
 package com.consertreservation.domain.usertoken.model;
 
 import static com.consertreservation.domain.usertoken.exception.UserTokenErrorCode.INVALID_WAITING_ORDER;
+import static com.consertreservation.domain.usertoken.exception.UserTokenErrorCode.UNAUTHORIZED;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.consertreservation.domain.usertoken.exception.UserTokenErrorCode;
@@ -22,5 +23,19 @@ class UserTokenTest {
         assertThatThrownBy(() -> userToken.updateWaitingOrder(-1))
                 .isInstanceOf(UserTokenException.class)
                 .hasFieldOrPropertyWithValue("errorCode", INVALID_WAITING_ORDER);
+    }
+    
+    @Test
+    @DisplayName("유저 상태값이 succss 가 아닌 경우 예외가 발생한다.")
+    void validate_authorization_test() {
+        // Given
+        UserToken userToken = UserToken.builder()
+                .build();
+
+        // When & Then
+        assertThatThrownBy(() -> userToken.validateAuthorization())
+                .isInstanceOf(UserTokenException.class)
+                .hasFieldOrPropertyWithValue("errorCode", UNAUTHORIZED);
+
     }
 }

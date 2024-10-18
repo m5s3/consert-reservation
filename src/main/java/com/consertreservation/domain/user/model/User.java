7d6ac71b37a480c73035348e70f8e5f52ecced8c
m5s3 <devmyoungsoo2@gmail.com>
2024-10-18 13:41:39 +0900

    fix: 충돌로 인해서 table 생성시 users 로 생성 되도록 수정

--- src/main/java/com/consertreservation/domain/user/model/User.java
+++ src/main/java/com/consertreservation/domain/user/model/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 public class User extends BaseTimeEntity {
 
     @Id
